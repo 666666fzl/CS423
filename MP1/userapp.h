460bc01a4ae598e645d8c8aa89c94ddaee528851
zfeng6 <zfeng6@illinois.edu>
2016-02-16 22:44:17 -0600

    asdf

---
+++ MP1/userapp.h
@@ -0,0 +1,12 @@
+#ifndef __USERAPP__
+#define __USERAPP__
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int reg(char* write_path);
+
+#endif
