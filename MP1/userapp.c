e51db3acd788d205ef1c9668e603e224eb97d13b
zfeng6 <zfeng6@illinois.edu>
2016-02-16 22:11:05 -0600

    userapp

--- MP1/userapp.c
+++ MP1/userapp.c
@@ -1,6 +1,30 @@
 #include "userapp.h"
 
+ // * params: char *write_path: registration file
+ // * return: on success, return the number of bytes written; on failure return a negative number
+int reg(char* write_path)
+{
+	FILE * fp = fopen (write_path, "a+");
+	if(!fp)
+	{
+		perror ("file doesn't exist\n");
+		return -1;
+	}
+	pid_t pid = getpid();
+	int byte_write = fprintf(fp, "%d\n", pid);
+	return byte_write;
+}
+
 int main(int argc, char* argv[])
 {
+	if(argc != 2)
+	{
+		perror("Number of arguments wrong\n");
+		return -1;
+	}
+
+	char *write_path = argv[1];
+	reg (write_path);
 	return 0;
 }
+
