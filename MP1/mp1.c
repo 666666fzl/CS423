93ea820e2da22fe09c2d34b64961c4d51a591e40
Andrew <andrewajsk@gmail.com>
2016-02-16 22:18:26 -0600

    Andrew's push

--- MP1/mp1.c
+++ MP1/mp1.c
@@ -10,6 +10,46 @@ MODULE_DESCRIPTION("CS-423 MP1");
 
 #define DEBUG 1
 
+typedef struct list_head{
+	void *myitem;
+	struct list_head * next;
+	struct list_head * prev;
+} list_head;
+
+typedef struct list{
+	list_head node;
+	int data;
+	void * voidP;
+}list_t;
+
+
+
+list_t * t;
+list_head tail;
+
+static ssize_t mp1_read(struct file *file, char __user * buffer, size_t count, loff_t * data){
+	int copied;
+	char * buf;
+	buf = (char*) kmalloc( count, GFP_KERNEL);
+	copied = 0;
+	
+	
+	copy_to_user(buffer, buf, copied);
+	kfree(buf);
+	return copied;
+}
+
+static ssize_t mp1_write(struct file *file, char __user *buffer, size_t count, loff_t * data){
+	char * buf = (char*)kmalloc(count. GFP_KERNEL);
+	copy_from_user(buf, buffer, count);
+
+	tail->next = kmalloc(sizeof(list_head), GFP_KERNEL);
+	tail->next->prev = tail;
+	tail = tail->next;
+	tail->myitem = buf;
+	
+}
+
 // mp1_init - Called when module is loaded
 int __init mp1_init(void)
 {
@@ -17,9 +57,16 @@ int __init mp1_init(void)
    printk(KERN_ALERT "MP1 MODULE LOADING\n");
    #endif
    // Insert your code here ...
-   // ANDREW's TEST
-   
-   
+//   proc_dir = proc_mkdir(DIRECTORY, NULL);
+//	proc_entry = proc_create(FILENAME, 0666, proc_dir, &mp1_file);
+  
+	list = kmalloc(sizeof(list), GFP_KERNEL);
+	list->voidP = NULL= NULL;
+	list->node.myitem = getpid();
+	list->node.next = NULL;
+	list->node.prev = NULL;
+ 	tail = &list->node;
+
    printk(KERN_ALERT "MP1 MODULE LOADED\n");
    return 0;   
 }
@@ -32,7 +79,7 @@ void __exit mp1_exit(void)
    #endif
    // Insert your code here ...
    
-   
+   freeEverything();
 
    printk(KERN_ALERT "MP1 MODULE UNLOADED\n");
 }
