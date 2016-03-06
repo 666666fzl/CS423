e76ce9a7a9f65a85a8c4a077f0e41d629f7f0f2c
Hao Zheng <hzheng19@illinois.edu>
2016-03-05 22:08:18 -0600

    hao can go to hell now

--- MP2/userapp.c
+++ MP2/userapp.c
@@ -3,7 +3,7 @@
 
  // * params: char *write_path: registration file
  // * return: on success, return the number of bytes written; on failure return a negative number
-int reg(char* write_path)
+int reg(char* write_path, char *period)
 {
     FILE * fp = fopen (write_path, "a+");
     if(!fp)
@@ -12,7 +12,7 @@ int reg(char* write_path)
         return -1; 
     }   
     pid_t pid = getpid();
-    int byte_write = fprintf(fp, "R:%d %d %d", pid, 1, 1);
+    int byte_write = fprintf(fp, "R:%d %s %d", pid, period, 1);
     fclose(fp);
     return byte_write;
 }
@@ -34,17 +34,18 @@ int unreg(char* write_path, pid_t pid)
 
 int main(int argc, char* argv[])
 {
-    if(argc < 3)
+    if(argc < 4)
     {   
         perror("Number of arguments wrong, please follow [filepath] [cmd] [args]\n");
         return -1; 
     }
 
     char *cmd = argv[2];
+	char *period = argv[3];
     char *write_path = argv[1];
 
     if (*cmd == 'r') {
-        reg (write_path);
+        reg (write_path, period);
     }
     else if (*cmd == 'd') {
         pid_t pid = atoi(argv[3]);
