94772e374f015274deff788b1d062950782eef13
Hao Zheng <hzheng19@illinois.edu>
2016-03-06 19:59:41 -0600

    fix delete_from_list, malloc new node using slab allocator

--- MP2/mp2.c
+++ MP2/mp2.c
@@ -28,7 +28,7 @@ typedef struct mp2_task_struct {
 	// 0 = SLEEPING
 	// 1 = READY
 	// 2 = RUNNING
-	int status;
+	int state;
 	unsigned long proc_time;
 	unsigned long period;
 	struct list_head process_node;
@@ -42,10 +42,7 @@ static struct mutex my_mutex;
 static struct kmem_cache *task_cache;
 
 LIST_HEAD(taskList);
-
-void slab_ctor(void *foo) {
-	return;
-}
+task_node_t *current_running_task;
 
 // Called when user application use "cat" or "fopen"
 // The function read the status file and print the information related out
@@ -95,7 +92,8 @@ int add_to_list(char *buf)
     task_node_t *entry;
 	int i = 0;
 	char *pch;
-	task_node_t *new_task = (task_node_t*)kmalloc(sizeof(task_node_t), GFP_KERNEL);
+	//task_node_t *new_task = (task_node_t*)kmalloc(sizeof(task_node_t), GFP_KERNEL);
+	task_node_t *new_task = kmem_cache_alloc(task_cache, GFP_KERNEL);
     char *dataHolder = (char*)kmalloc(strlen(buf)+1, GFP_KERNEL);
 	if(dataHolder)
 	{
@@ -121,7 +119,7 @@ int add_to_list(char *buf)
 		pch = strsep(&dataHolder, " ,");
 	}	
 	
-	new_task -> status = 0;
+	new_task -> state = 0;
 
     list_for_each(pos, &taskList) {
         entry = list_entry(pos, task_node_t, process_node);
@@ -140,60 +138,99 @@ int delete_from_list(char *pid)
     struct list_head *pos;
     struct list_head *next;
     task_node_t *curr;
+	char curr_pid[20];
     mutex_lock(&my_mutex);
 
- /*   list_for_each_safe(pos, next, &taskList){
-        curr = list_entry(pos, list_node_t, node);
-        if(strcmp(curr->data, pid)==0)
+    list_for_each_safe(pos, next, &taskList){
+        curr = list_entry(pos, task_node_t, process_node);
+        memset(curr, 0, 20);
+		sprintf(curr_pid, "%u", curr->pid);
+		if(strcmp(curr_pid, pid)==0)
         {
             list_del(pos);
-            kfree(list_entry(pos, list_node_t, node));
+            kmem_cache_free(task_cache, list_entry(pos, task_node_t, process_node));
         }
     }
-*/
+
     mutex_unlock(&my_mutex);
-    return 0;
+    return -1;
+}
+
+void dispatching_thread(void)
+{
+	task_node_t *next_task;
+	task_node_t *entry;
+	task_node_t *prev_task;
+	struct sched_param new_sparam; 
+	struct sched_param old_sparam; 
+	struct list_head *pos;
+	if(current_running_task)
+	{
+		list_for_each(pos, &taskList) {
+			entry = list_entry(pos, task_node_t, process_node);
+			if (entry->period < current_running_task->period && entry->state==1) {
+				next_task = entry;
+				break;
+			}
+		}
+	}
+	
+	prev_task = current_running_task;
+	prev_task->state = 1;
+	
+	//old task
+	old_sparam.sched_priority=0; 
+	sched_setscheduler(prev_task->linux_task, SCHED_NORMAL, &old_sparam);
+	
+	// new task
+	wake_up_process(next_task->linux_task); 
+	new_sparam.sched_priority=99;
+	sched_setscheduler(next_task->linux_task, SCHED_FIFO, &new_sparam);
+
+	current_running_task = next_task;
+	current_running_task->state=2;
 }
 
 int admission_control(struct file *file, const char __user *buffer, size_t count, loff_t * data)
 {
-
-    char * buf = (char*)kmalloc(count+1, GFP_KERNEL);
-    int ret = -1;
-
-    if (count > MAX_BUF_SIZE - 1) {
-        count = MAX_BUF_SIZE - 1;
-    }
-
-
-    copy_from_user(buf, buffer, count);
-
-    buf[count]='\0';
-    printk(KERN_ALERT "mp2_write called, pid: %s", buf);
-
-    // Check the starting char of buf, if:
-    // 1) register: R,PID,PERIOD,COMPUTATION
-    if (buf[0] == 'R') {
-        ret = add_to_list(buf+2);
-    }
-    else if (buf[0] == 'Y'){
-    // 2) yield: Y,PID 
-    }
-    else if (buf[0] == 'D') {
-    // 3) unregister: D,PID
-        ret = delete_from_list(buf+2);
-    }
-    else {
-        return 0;
-    }
-    return ret;
+    return -1;
 }
 
 // Called when user application registered a process
 // The function get the pid from the user and put it on the linked list, which actually write it in the status file
 static ssize_t mp2_write(struct file *file, const char __user *buffer, size_t count, loff_t * data){
-    int ret = admission_control(file, buffer, count, data);
-    return ret;
+	char * buf = (char*)kmalloc(count+1, GFP_KERNEL);
+	int ret = -1;
+
+
+	if (count > MAX_BUF_SIZE - 1) {
+		count = MAX_BUF_SIZE - 1;
+	}
+
+	copy_from_user(buf, buffer, count);
+	buf[count] = '\0';
+
+	printk(KERN_ALERT "MP2_WRITE CALLED, INPUT:%s\n", buf);
+	
+	// Check the starting char of buf, if:
+	// 1.register: R,PID,PERIOD,COMPUTATION
+	if (buf[0] == 'R') {
+		ret = add_to_list(buf+2);
+		printk(KERN_ALERT "REGISTERED PID:%s", buf+2);
+	}
+	else if (buf[0] == 'Y') {
+	// 2.yield: Y,PID
+	}
+	else if (buf[0] == 'D') {
+	// 3.unregister: D,PID
+		ret = delete_from_list(buf+2);
+		printk(KERN_ALERT "UNREGISTERED PID: %s", buf+2);
+	}
+	else {
+		return 0;
+	}
+
+	return ret;
 }
 
 // Get the scheduled work and work on updating the cpu use time for processes corrsponding to nodes on the linked list
@@ -258,7 +295,7 @@ int __init mp2_init(void)
     // create proc directory and file entry
     proc_dir = proc_mkdir(DIRECTORY, NULL);
     proc_entry = proc_create(FILENAME, 0666, proc_dir, & mp2_file);
-
+	current_running_task = NULL;
     // create work queue
     //update_workqueue = create_workqueue("update_workqueue");
 
@@ -266,7 +303,7 @@ int __init mp2_init(void)
     //_create_my_timer();
 
 	// create cache for slab allocator
-	task_cache = kmem_cache_create("task_cache", sizeof(task_node_t), 0, SLAB_HWCACHE_ALIGN, slab_ctor);
+	task_cache = kmem_cache_create("task_cache", sizeof(task_node_t), 0, SLAB_HWCACHE_ALIGN, NULL);
 
     // init mutex lock
     mutex_init(&my_mutex);
@@ -293,11 +330,11 @@ void __exit mp2_exit(void)
     destroy_workqueue(update_workqueue);
 
     // remove every node on linked list and remove the list     
-  /*  list_for_each_safe(pos, next, &taskList){
+    list_for_each_safe(pos, next, &taskList){
         list_del(pos);
-        kfree(list_entry(pos, list_node_t, node));
-    }
-*/
+    	kmem_cache_free(task_cache, list_entry(pos, task_node_t, process_node));
+	}
+
     // remove file entry and repository  
     remove_proc_entry(FILENAME, proc_dir);
     remove_proc_entry(DIRECTORY, NULL);
