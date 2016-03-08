211d82107167822a91b503d4f6c5212326395092
Hao Zheng <hzheng19@illinois.edu>
2016-03-07 22:26:40 -0600

    userapp TODO(Zhili): real proc time, read status function, what is jobs?

--- MP2/userapp.c
+++ MP2/userapp.c
@@ -1,27 +1,30 @@
 #include "userapp.h"
 #include <stdlib.h>
 
- // * params: char *write_path: registration file
+#define PERIOD 3000
+#define PROC_TIME 1000
+#define PROC_FILE "/proc/mp2/status"
+
+ // * params: the pid, period(ms) and process time(ms) for the task to be registered
  // * return: on success, return the number of bytes written; on failure return a negative number
-int reg(char* write_path, char *period)
+int reg(pid_t pid, unsigned long period, unsigned long proc_time)
 {
-    FILE * fp = fopen (write_path, "a+");
+    FILE * fp = fopen (PROC_FILE, "a+");
     if(!fp)
     {   
         perror ("file doesn't exist\n");
         return -1; 
     }   
-    pid_t pid = getpid();
-    int byte_write = fprintf(fp, "R:%d %s %d", pid, period, 1);
+    int byte_write = fprintf(fp, "R:%d %lu %lu", pid, period, proc_time);
     fclose(fp);
     return byte_write;
 }
 
  // * params: char *write_path: registration file
  // * return: on success, return the number of bytes written; on failure return a negative number
-int unreg(char* write_path, pid_t pid)
+int unreg(pid_t pid)
 {
-    FILE * fp = fopen (write_path, "a+");
+    FILE * fp = fopen (PROC_FILE, "a+");
     if(!fp)
     {   
         perror ("file doesn't exist\n");
@@ -32,9 +35,9 @@ int unreg(char* write_path, pid_t pid)
     return byte_write;
 }
 
-int yield(char* write_path, pid_t pid)
+int yield(pid_t pid)
 {
-	FILE * fp = fopen(write_path, "a+");
+	FILE * fp = fopen(PROC_FILE, "a+");
 	if(!fp) {
 		perror("file doesn't exist\n");
 		return -1;
@@ -44,9 +47,14 @@ int yield(char* write_path, pid_t pid)
 	return byte_write;
 }
 
+int read_status(void)
+{
+
+}
+
 int main(int argc, char* argv[])
 {
-    if(argc < 4)
+    /*if(argc < 4)
     {   
         perror("Number of arguments wrong, please follow [filepath] [cmd] [args]\n");
         return -1; 
@@ -68,5 +76,17 @@ int main(int argc, char* argv[])
 		yield (write_path, pid);
     }
     while(1);
-    return 0;
+    return 0;*/
+    pid_t pid = getpid();
+    reg(pid, PERIOD, PROC_TIME); //Proc filesystem
+    list = read_status(); //Proc filesystem: Verify the process was admitted 
+    if (!process in the list) exit 1;
+    //setup everything needed for real-time loop: t0=gettimeofday() 
+    yield(pid); //Proc filesystem
+    //this is the real-time loop
+    while(exist jobs)
+    {
+    do_job(); //wakeup_time=gettimeofday()-t0 and factorial computation
+    YIELD(PID); //Proc filesystem. JobProcessTime=gettimeofday()-wakeup_time }
+    UNREGISTER(PID); //Proc filesystem }
 }
