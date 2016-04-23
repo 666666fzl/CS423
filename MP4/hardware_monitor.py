b71c45901bb12398c8c5ef39d4cd5e1e1c4f41c9
Hao Zheng <hzheng19@illinois.edu>
2016-04-23 16:49:56 -0500

    hardware monitor ver1

---
+++ MP4/hardware_monitor.py
@@ -0,0 +1,13 @@
+import psutil # Import psutil module for cpu info
+
+class HardwareMonitor:
+	def __init__(self, user_input_throttling):
+		self.throttling = user_input_throttling
+		self.cpu_use = 0; # default
+
+	def get_cpu_info(self):
+		self.cpu_use = psutil.cpu_percent(1) # CPU times elapsed before and after 1 sec
+		return self.cpu_use
+
+	def get_throttling(self):
+        return self.cpu_throttling
