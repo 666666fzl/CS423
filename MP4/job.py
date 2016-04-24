5d0c2b4db769b40654589b884d73d697bdde0f04
Hao Zheng <hzheng19@illinois.edu>
2016-04-23 23:53:53 -0500

    asdf

--- MP4/job.py
+++ MP4/job.py
@@ -1,9 +1,9 @@
 class Job(object):
 	def __init__(self, start_idx, length, data_vector):
-    	self.id = start_idx
-    	self.data_vector = data_vector
-    	self.start_idx = start_idx
-    	self.length = length
+		self.id = start_idx
+		self.data_vector = data_vector
+		self.start_idx = start_idx
+		self.length = length
 
 	def compute(self):
 		for i in xrange(self.length):
